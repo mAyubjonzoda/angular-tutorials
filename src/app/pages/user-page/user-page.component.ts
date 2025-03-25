be2e28c0cc66d8e4157cce97bd6f010e19d53ee2
Ayubjonzoda Muhammad <m.ayubjonzoda@gmail.com>
2025-03-25 09:36:54 +0500

    finished

--- src/app/pages/user-page/user-page.component.ts
+++ src/app/pages/user-page/user-page.component.ts
@@ -1,9 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { CarService } from '../../services/car.service';
+import {
+  CurrencyPipe,
+  DatePipe,
+  DecimalPipe,
+  LowerCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
+import { StarPipe } from '../../pipes/star.pipe';
+import { ReversePipe } from '../../pipes/reverse.pipe';
 
 @Component({
   selector: 'app-user-page',
-  imports: [],
+  imports: [
+    LowerCasePipe,
+    UpperCasePipe,
+    DecimalPipe,
+    DatePipe,
+    CurrencyPipe,
+    StarPipe,
+    ReversePipe,
+  ],
   templateUrl: './user-page.component.html',
 })
 export class UserPageComponent {
@@ -12,4 +29,9 @@ export class UserPageComponent {
   constructor() {
     this.display = this.carService.getCars().join(' ⭐️ ');
   }
+  num = 1103.12345;
+  birthday = new Date(2023, 3, 2);
+  cost = 4560.34;
+  name = 'Muhammad';
+  word = 'You are a champion';
 }
